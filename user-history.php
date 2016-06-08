f61ef659395eadb0952a0453f27de92009f1709d
muhizi <muhizia>
2016-06-08 12:28:32 +0200

    display folder viewed and remove repetition of name displayed

--- user-history.php
+++ user-history.php
@@ -8,12 +8,17 @@ if($_REQUEST['user_Id']){
 
 <div class="content-box-large">
   				<div class="panel-heading">
-					<div class="panel-title">Last action performed daily</div>
+					<?php
+						$stmt_user = $DB_con->prepare("select * from user where user_Id=:user_id");
+						$stmt_user->execute(array(':user_id'=>$id));
+						$row_user=$stmt_user->fetch(PDO::FETCH_ASSOC);
+					?>
+					<div class="panel-title"><h3>Action history performed by: <?php echo strtoupper(substr($row_user['Firstname'],0,1)).strtolower(substr($row_user['Firstname'],1,20))."\t".strtoupper(substr($row_user['Lastname'],0,1));  ?>.</h3></div>
 				</div>
   				<div class="panel-body">
 				  <?php
-									  $stmt = $DB_con->prepare("select * from user right join(doc_sessions left join (actions_doc join upload_doc on actions_doc.upl_doc_id=upload_doc.id) on doc_sessions.sess_id = actions_doc.session_Id) on user.user_Id=doc_sessions.user_Id where user.user_Id=:user_id");
-                                      $count_stmt = $DB_con->prepare("SELECT count(*) cat_num FROM user right join(doc_sessions left join (actions_doc join upload_doc on actions_doc.upl_doc_id=upload_doc.id) on doc_sessions.sess_id = actions_doc.session_Id) on user.user_Id=doc_sessions.user_Id where user.user_Id=:user_id");
+									  $stmt = $DB_con->prepare("select * from doc_sessions left join (actions_doc join (upload_doc join organization on upload_doc.org_id=organization.id) on actions_doc.upl_doc_id=upload_doc.id) on doc_sessions.sess_id = actions_doc.session_Id where doc_sessions.user_Id=:user_id order by time_in desc");
+                                      $count_stmt = $DB_con->prepare("SELECT count(*) cat_num FROM doc_sessions left join (actions_doc join (upload_doc join organization on upload_doc.org_id=organization.id) on actions_doc.upl_doc_id=upload_doc.id) on doc_sessions.sess_id = actions_doc.session_Id where doc_sessions.user_Id=:user_id");
 									  $stmt->execute(array(':user_id'=>$id));
 									  $count_stmt->execute(array(':user_id'=>$id));
 									  $count_row=$count_stmt->fetch(PDO::FETCH_ASSOC);
@@ -25,13 +30,13 @@ if($_REQUEST['user_Id']){
 											  
 						<table class="table table-striped">
 			              <thead>
-			                <tr>
+							<tr>
 			                  <th>#</th>
-			                  <th>username</th>
-                              <th>Last session Time in</th>
+			                  <th>Last session Time in</th>
                               <th>Last session Time out</th>
                               <th>Done actions time</th>
-							  <th>On file action</th>
+							  <th>Organization</th>
+                              <th>Files</th>
 							  <th></th>
 			                </tr>
 			              </thead>
@@ -41,10 +46,10 @@ if($_REQUEST['user_Id']){
                                                                       $name=substr($row['Doc_name'],0,30);
 																	  echo "<tr>
 																			<td>{$i}</td>
-																			<td>{$row['username']}</td>
-                                                                            <td>{$row['time_in']}</td>
+																			<td>{$row['time_in']}</td>
                                                                             <td>{$row['time_out']}</td>
 																			<td>{$row['tim_done']}</td>
+                                                                            <td>{$row['organization']}</td>
                                                                             <td>$name</td>
 																			<td>{$row['action_doc']}</td></tr>";
                                                                         $i++;
